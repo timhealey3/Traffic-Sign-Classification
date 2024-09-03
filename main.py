444840c36c0c4d3f92709b504e387e63fc630650
Timothy Healey <87385121+timhealey3@users.noreply.github.com>
2024-09-02 23:11:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import lenet
+import test
 
-programRunning = True
-while programRunning:
-    programRunning = False
+def displayText():
+    print("Welcome to Traffic Sign Classifier")
+    user_input = input("Would you like to Train the Model? y/n: ")
+    return user_input
+
+def trainModel():
+    model = lenet.modifiedModel()
+    lenet.trainModel(model)
+    return model
+
+def testModel(model):
+    test.testModelMain(model)
+
+def main():
+    program_running = True
+    while(program_running):
+        user_input = displayText()
+        if (user_input == "y" or user_input == "Y"):
+            model = trainModel()
+            testModel(model)
+            program_running = False
+        if (user_input == "n" or user_input =="N"):
+            program_running = False
+
+main()
